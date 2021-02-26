264eda0a733fd5fc809e93fea3f7cd1f5f365cd1
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-02-26 15:21:58 -0800

    deleted articles from app

--- lib/providers/articles.dart
+++ lib/providers/articles.dart
@@ -8,162 +8,162 @@ import '../models/article.dart';
 class Articles with ChangeNotifier {
   List<Article> _articles = [
     //Experiment 1
-    Article(
-      id: 'p1',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/q0dF5Ws/E1P1.jpg',
-    ),
-    Article(
-      id: 'p1',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/yh8xmw2/E1P2.jpg',
-    ),
-    Article(
-      id: 'p1',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/W5NG03F/E1P3.jpg',
-    ),
-    Article(
-      id: 'p1',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/Gcchh7w/E1P4.jpg',
-    ),
+    // Article(
+    // id: 'p1',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/q0dF5Ws/E1P1.jpg',
+    // ),
+    // Article(
+    // id: 'p1',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/yh8xmw2/E1P2.jpg',
+    // ),
+    // Article(
+    // id: 'p1',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/W5NG03F/E1P3.jpg',
+    // ),
+    // Article(
+    // id: 'p1',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/Gcchh7w/E1P4.jpg',
+    // ),
 
 //Experiment 2
 
-    Article(
-      id: 'p2',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/F02yy3W/E2P1.jpg',
-    ),
-    Article(
-      id: 'p2',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/grmfHwj/E2P2.jpg',
-    ),
+    // Article(
+    // id: 'p2',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/F02yy3W/E2P1.jpg',
+    // ),
+    // Article(
+    // id: 'p2',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/grmfHwj/E2P2.jpg',
+    // ),
 
     //Experiment 3
 
-    Article(
-      id: 'p3',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/T84frHz/E3P1.jpg',
-    ),
-    Article(
-      id: 'p3',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/gtMvcbc/E3P2.jpg',
-    ),
+    // Article(
+    // id: 'p3',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/T84frHz/E3P1.jpg',
+    // ),
+    // Article(
+    // id: 'p3',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/gtMvcbc/E3P2.jpg',
+    // ),
 
     //Experiment 4
 
-    Article(
-      id: 'p4',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/kXNnzgK/E4P1.jpg',
-    ),
-    Article(
-      id: 'p4',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/gg7rBvb/E4P2.jpg',
-    ),
-
+    // Article(
+    // id: 'p4',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/kXNnzgK/E4P1.jpg',
+    // ),
+    // Article(
+    // id: 'p4',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/gg7rBvb/E4P2.jpg',
+    // ),
+//
     //Experiment 5
 
-    Article(
-      id: 'p5',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/DVctyKb/E5P1.jpg',
-    ),
-    Article(
-      id: 'p5',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/v18LS1N/E5P2.jpg',
-    ),
-    Article(
-      id: 'p5',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/FWT47pY/E5P3.jpg',
-    ),
-    Article(
-      id: 'p5',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/NYWNkw3/E5P4.jpg',
-    ),
+    // Article(
+    // id: 'p5',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/DVctyKb/E5P1.jpg',
+    // ),
+    // Article(
+    // id: 'p5',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/v18LS1N/E5P2.jpg',
+    // ),
+    // Article(
+    // id: 'p5',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/FWT47pY/E5P3.jpg',
+    // ),
+    // Article(
+    // id: 'p5',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/NYWNkw3/E5P4.jpg',
+    // ),
 
     //Experiment 6
 
-    Article(
-      id: 'p6',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/8g4W9Yt/E6P1.jpg',
-    ),
-    Article(
-      id: 'p6',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/pbQjKxZ/E6P2.jpg',
-    ),
+    // Article(
+    // id: 'p6',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/8g4W9Yt/E6P1.jpg',
+    // ),
+    // Article(
+    // id: 'p6',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/pbQjKxZ/E6P2.jpg',
+    // ),
 
     //Experiment 7
 
-    Article(
-      id: 'p7',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/zfkKgSJ/E7P1.jpg',
-    ),
-    Article(
-      id: 'p7',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/tLs4wgZ/E7P2.jpg',
-    ),
+    // Article(
+    // id: 'p7',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/zfkKgSJ/E7P1.jpg',
+    // ),
+    // Article(
+    // id: 'p7',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/tLs4wgZ/E7P2.jpg',
+    // ),
 
     //Experiment 8
-    Article(
-      id: 'p8',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/h1WPdMc/E8P1.jpg',
-    ),
-    Article(
-      id: 'p8',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/LhPkKjV/E8P2.jpg',
-    ),
-    Article(
-      id: 'p8',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/y5kYCnC/E8P3.jpg',
-    ),
+    // Article(
+    // id: 'p8',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/h1WPdMc/E8P1.jpg',
+    // ),
+    // Article(
+    // id: 'p8',
+    // level: '100',/
+    // imageUrl: 'https://i.ibb.co/LhPkKjV/E8P2.jpg',/
+    // ),
+    // Article(
+    // id: 'p8',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/y5kYCnC/E8P3.jpg',
+    // ),
 
     //Experiment 9
 
-    Article(
-      id: 'p9',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/XYXpW4F/E9P1.jpg',
-    ),
-    Article(
-      id: 'p9',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/BnvYYnv/E9P2.jpg',
-    ),
-    Article(
-      id: 'p9',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/MCFgsfX/E9P3.jpg',
-    ),
-
+    // Article(
+    // id: 'p9',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/XYXpW4F/E9P1.jpg',
+    //),
+    // Article(
+    // id: 'p9',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/BnvYYnv/E9P2.jpg',
+    // ),
+    // Article(
+    // id: 'p9',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/MCFgsfX/E9P3.jpg',
+    // ),
+//
     //Experiment 10
 
-    Article(
-      id: 'p10',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/GFKSgDV/E10P1.jpg',
-    ),
-    Article(
-      id: 'p10',
-      level: '100',
-      imageUrl: 'https://i.ibb.co/80RzB1s/E10P2.jpg',
-    ),
+    // Article(
+    // id: 'p10',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/GFKSgDV/E10P1.jpg',
+    // ),
+    // Article(
+    // id: 'p10',
+    // level: '100',
+    // imageUrl: 'https://i.ibb.co/80RzB1s/E10P2.jpg',
+    // ),
   ];
 
   List<Article> get articles {
